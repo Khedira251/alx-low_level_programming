058d997340da362f6566799adbcba91950bd07c4
Khedira251 <abdulkhedir1@gmail.com>
2023-06-15 17:23:53 +0300

    low level updated

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - a program that print a line using printf function
+ * main - main block
  * Return: 0
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
-Return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
